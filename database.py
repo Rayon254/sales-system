e25208ccad2cae1d1eb8a92e64f7723de7f79f05
Rayon254 <150139046+Rayon254@users.noreply.github.com>
2024-03-18 11:22:52 +0300

    updated

--- database.py
+++ database.py
@@ -20,7 +20,16 @@ def get_products():
 
 get_products()
 
-#write a function to get sales
+# delete a product
+
+# def delete_product():
+#     delete_query = "DELETE FROM products WHERE id = %s;"
+#     curr.execute(delete_query, (product_id,))
+#     conn.commit()
+
+# delete_product()
+
+#write a function to get sales define a cfunction (def)
 
 def calc_sales():
     curr.execute("select * FROM sales")
@@ -28,15 +37,14 @@ def calc_sales():
     for sale in sales:
         print(sale)
 
-calc_sales()
+# calc_sales()
 
 def get_data(table_name):
     select=f"select * from {table_name};"
-    curr.execute(select)  # contains the sql querry you want to run
+    curr.execute(select)  # contains the sql querry you want to run   curr=cursor.execute
     data=curr.fetchall()
     return(data)
         
-
 
 
 # insert data
@@ -47,9 +55,49 @@ def insert_products(values):
     curr.execute(insert,values)
     conn.commit()
 
-product_value=("tilapia",200,500,8)
-insert_products(product_value)
-get_data('sales')
-get_data('products')
+# items=("tilapia",200,500,8)
+# insert_product(items)
+
 
 # insert sales
+def insert_sales(values):
+    insert="insert into sales(pid,quantity,created_at)values(%s,%s,now());"
+
+    curr.execute(insert,values)
+    conn.commit()
+
+#sales per product
+def sales_product():
+    display="select name,sum(selling_price*quantity) FROM products join sales on products.id=sales.pid Group by name;"
+    curr.execute(display)
+    data=curr.fetchall()
+    return(data)
+
+# profit per product
+
+def profit_product():
+    profit="select name,sum(selling_price-buying_price) FROM products Group by name;"
+    curr.execute(profit)
+    data=curr.fetchall()
+    return(data)
+
+# sales per day
+
+def daily_sales():
+    dailysales="select date(sales.created_at) as day,sum(selling_price*quantity) FROM products join sales on products.id=sales.pid Group by day Order by day;"
+    curr.execute(dailysales)
+    data=curr.fetchall()
+    return(data)
+
+# profit per day
+
+def daily_profit():
+    profit="select date(sales.created_at) as day, sum(selling_price-buying_price) FROM products join sales on products.id=sales.pid Group by day Order by day;"
+    curr.execute(profit)
+    data=curr.fetchall()
+    return(data)
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,105 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request, redirect, url_for
+from database import insert_products, get_data, insert_sales, sales_product, profit_product, daily_sales, daily_profit
+
 
 #create a flask instance
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def hello():
     return render_template('index.html')
 
 @app.route('/home')
 def home():
     return "home page"
+
+@app.route('/products')
+def products():
+    prods=get_data("products")
+    return render_template("products.html",products=prods)
+
+@app.route('/sales')
+def sales():
+    sale=get_data("sales")
+    return render_template('sales.html',sales=sale)
+
+
+@app.route('/products', methods=['POST'])
+def add_products():
+
+        product_name = request.form['product_name']
+        buying_price = request.form['buying_price']
+        selling_price = request.form['selling_price']
+        stock_quantity = request.form['stock_quantity']
+        
+        value=product_name, buying_price, selling_price, stock_quantity
+        
+        insert_products(value)
+
+        return redirect(url_for('products'))        
+
+
+
+@app.route('/sales', methods=['GET'])
+def sale():
+    sales=get_data("sales")
+    products=get_data("products")
+    return render_template('sales.html',sales=sales,products=products)
+
+
+@app.route('/make_sales', methods=['GET','POST'])
+def make_sales():
+       
+        pid=request.form['pid']
+        quantity=request.form['quantity']
+        sale=(pid,quantity)
+
+        insert_sales(sale)
+        return redirect(url_for('sale'))
+
+
+@app.route('/dashboard')
+def dashboard():
+    sp=sales_product()
+    names=[]
+    value=[]
+    for i in sp:
+        names.append(str(i[0]))
+        value.append(float(i[1]))
+
+  
+    pp=profit_product()
+    names=[]
+    profit=[]
+    for i in pp:
+
+        names.append(str(i[0]))
+        profit.append(float(i[1]))
+
+        
+    #sales per day on a line chart 
+
+    ds=daily_sales()
+    day=[]
+    sl=[]
+
+    for i in ds:
+        day.append(str(i[0]))
+        sl.append(float(i[1]))
+
+    dp=daily_profit()
+    day=[]
+    profit=[]
+
+    for i in dp:
+        day.append(str(i[0]))
+        profit.append(float(i[1]))
+
+    return render_template('dashboard.html',names=names,value=value, profit=profit, day=day,sl=sl)
+ 
+
+
 
 
 app.run(debug=True)
